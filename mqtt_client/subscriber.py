03e38d830b470c67b210a83bb6703d99e331a961
Andreas Thoeni <ta2244@mci4me.at>
2024-06-14 13:13:00 +0200

    database added

--- mqtt_client/subscriber.py
+++ mqtt_client/subscriber.py
@@ -1,4 +1,7 @@
 import paho.mqtt.client as mqtt
+import json
+from tinydb import TinyDB, Query
+db = TinyDB('../database/db.json')
 
 # MQTT broker details
 broker = "158.180.44.197"
@@ -10,30 +13,55 @@ topic = "iot1/teaching_factory_fast/"
 subT_disp_red = "dispenser_red"
 subT_disp_green = "dispenser_green"
 subT_disp_blue = "dispenser_blue"
-
+subT_recipe = "recipe"
+subT_temp = "temperature"
+subT_scale = "scale"
+subT_drop_vibr = "drop_vibration"
+subT_ground_truth = "ground_truth"
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 # Set username and password for the MQTT broker
 mqttc.username_pw_set("bobm", "letmein")
 
+def store_data(payload_dict, topic):
+    if not db.table(topic):
+        db.table(topic)
+    db.table(topic).insert(payload_dict)
+
+
 # Define the callback function for when a message is received
 def on_message(client, userdata, message):
     print("Message received:")
     print("Topic: ", message.topic)
-    print("Payload: ", message.payload.decode())
+    try:
+        # Try to decode the payload and convert to a dictionary
+        payload_dict = json.loads(message.payload.decode())
+        print("Payload as dictionary: ", payload_dict)
+        print(type(payload_dict))
+
+        store_data(payload_dict, message.topic)
+
+    except json.JSONDecodeError:
+        # If decoding fails, print an error message
+        print("Failed to decode JSON payload.")
     print("\n")
-    print(message.payload)
 
 # Assign the callback function to the MQTT client
 mqttc.on_message = on_message
 
-
-#print("topic_groupname: ", topic_groupname)
 # Connect to the MQTT broker
 mqttc.connect(broker, port)
 
 # Subscribe to the topic
 mqttc.subscribe(topic + subT_disp_red)
+mqttc.subscribe(topic + subT_disp_green)
+mqttc.subscribe(topic + subT_disp_blue)
+mqttc.subscribe(topic + subT_recipe)
+mqttc.subscribe(topic + subT_temp)
+mqttc.subscribe(topic + subT_scale)
+mqttc.subscribe(topic + subT_drop_vibr)
+mqttc.subscribe(topic + subT_ground_truth)
+
 # Start the loop to process incoming messages
 mqttc.loop_start()
 
--- visualisierung/streamlit.py
+++ visualisierung/streamlit.py
@@ -1,42 +1,8 @@
 import streamlit as st
 import pandas as pd
+from tinydb import TinyDB, Query
 
-st.set_page_config(page_title="Sensor Data Display", page_icon="📊"
-                   , layout="wide", initial_sidebar_state="expanded")
+Data_Queue = Queue()
 
-st.title("Sensor Data Display")
 
-# Load the CSV file
-@st.cache
-def load_data(file_path):
-    data = pd.read_csv(file_path)
-    return data
-
-# Streamlit app layout
-def main():
-    st.title("Sensor Data Display")
-
-    # File upload
-    file = st.file_uploader("Upload CSV", type=["csv"])
-
-    if file is not None:
-        data = load_data(file)
-        
-        # Display the raw data
-        st.subheader("Raw Sensor Data")
-        st.write(data)
-
-        # Show a summary of the data
-        st.subheader("Data Summary")
-        st.write(data.describe())
-        
-        # Select sensor to visualize
-        sensor_options = data.columns[1:]  # Exclude the time column
-        selected_sensor = st.selectbox("Select Sensor to Display", sensor_options)
-        
-        # Plot the selected sensor data
-        st.subheader(f"{selected_sensor} Data")
-        st.line_chart(data.set_index('time')[selected_sensor])
-
-if __name__ == "__main__":
-    main()
+def get_data_from_topic(topic):
