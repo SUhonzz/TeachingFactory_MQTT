f6c684f67523579fc553e0d65145e6d4fc21efe3
ps4676 <ps4676@mci4me.at>
2024-06-18 15:39:39 +0200

    regresion model and 3d scatter plot

--- visualisierung/plotly_visualization.py
+++ visualisierung/plotly_visualization.py
@@ -1,11 +1,63 @@
+"""#Line-Chart
 import plotly.express as px
-
 df = px.data.gapminder().query("country=='Canada'")
 fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
 fig.show()
 #%%
+#Two-Line-Chart
 import plotly.express as px
 
 df = px.data.gapminder().query("continent=='Oceania'")
 fig = px.line(df, x="year", y="lifeExp", color='country')
+fig.show()"""
+#%%
+#Scatter Plots
+import plotly.express as px
+df = px.data.iris()
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color='petal_length')
+fig.show() 
+
+
+#%%
+#Linear Regression 
+import plotly.express as px
+
+df = px.data.tips()
+fig = px.scatter(df, x="total_bill", y="tip", trendline="ols")
+fig.show()
+
+
+#%%
+import plotly.graph_objects as go
+
+# Create random data with numpy
+import numpy as np
+np.random.seed(1)
+
+N = 100
+random_x = np.linspace(0, 1, N)
+random_y0 = np.random.randn(N) + 5
+random_y1 = np.random.randn(N)
+random_y2 = np.random.randn(N) - 5
+
+fig = go.Figure()
+
+# Add traces
+fig.add_trace(go.Scatter(x=random_x, y=random_y0,
+                    mode='markers',
+                    name='markers'))
+fig.add_trace(go.Scatter(x=random_x, y=random_y1,
+                    mode='lines+markers',
+                    name='lines+markers'))
+fig.add_trace(go.Scatter(x=random_x, y=random_y2,
+                    mode='lines',
+                    name='lines'))
+
+fig.show()
+#%%
+#3D Scatter Plot
+import plotly.express as px
+df = px.data.iris()
+fig = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width',
+              color='species')
 fig.show()
