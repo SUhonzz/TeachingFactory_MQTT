458325046076ddf516a01fb8b4f30b71c93ce54f
ps4676 <ps4676@mci4me.at>
2024-06-18 15:17:02 +0200

    plotly_visualization

---
+++ visualisierung/plotly_visualization.py
@@ -0,0 +1,11 @@
+import plotly.express as px
+
+df = px.data.gapminder().query("country=='Canada'")
+fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+fig.show()
+#%%
+import plotly.express as px
+
+df = px.data.gapminder().query("continent=='Oceania'")
+fig = px.line(df, x="year", y="lifeExp", color='country')
+fig.show()
