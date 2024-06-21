6c46c344d01f5e74f68481228bf1fbb843980c7c
ajayhanssen <andi.thoeni@gmail.com>
2024-06-21 09:54:40 +0200

    regression fertig, auch mit fremddaten

--- regression.py
+++ regression.py
@@ -9,7 +9,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
 
 # read data from X.csv to predict
-test_data = pd.read_csv('./database/X.csv')
+test_data = pd.read_csv('./database/X.csv', index_col=0)
 #print(test_data.head())
 
 # Read data to train on
@@ -17,7 +17,16 @@ data = pd.read_csv('./database/combined.csv')
 
 X = data.drop(['bottle', 'final_weight'], axis=1)
 y = data['final_weight']
-#print(X.head())
+print(X.head())
+
+
+test_data = test_data.drop(['temperature_mean_C'], axis=1)
+test_data = test_data.rename(columns={'vibration-index_red_vibration':'vibration-index_red',
+                                      'vibration-index_blue_vibration':'vibration-index_blue',
+                                      'vibration-index_green_vibration':'vibration-index_green'})
+test_data = test_data.reindex(columns=['fill_level_grams_red', 'fill_level_grams_blue', 'fill_level_grams_green', 'vibration-index_red', 'vibration-index_blue', 'vibration-index_green'])
+print(test_data.head())
+
 
 #-------------Excluding the fill levels for the different colors----------------
 #X = X.drop(['fill_level_grams_red', 'fill_level_grams_blue', 'fill_level_grams_green'], axis=1)
@@ -52,8 +61,7 @@ for model in models:
     print(F"Mean Squared Error Training of {model}: {mean_squared_error(y_train, model.predict(X_train))}")
     print("\n")
 
-test_data = test_data.drop(['temperature_mean_C'], axis=1)
-
-test_data = test_data.reindex(columns=['fill_level_grams_red', 'fill_level_grams_blue', 'fill_level_grams_green', 'vibration-index_red', 'vibration-index_blue', 'vibration-index_green'])
 test_data_predict = lin_model.predict(test_data)
-#test_data_predict.to_csv('./database/52216067-62200066-.csv', index=False)
+#test_predicted_df = pd.DataFrame(test_data_predict, columns=['Flaschen ID','y_hat'])
+test_predicted_df = pd.DataFrame({'Flaschen ID': test_data.index+1, 'y_hat': test_data_predict})
+test_predicted_df.to_csv('./database/52216067-62200066-61901292.csv', index=False)
